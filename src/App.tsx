03d1208870cedc1bde0767cd37646968b884c259
WilliamDK64 <williamdk.101@gmail.com>
2025-08-18 23:02:40 +1200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import title from "./assets/title.png";
+import pookie from "./assets/pookiegrab.png";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        {/*<img src={title} className="size-52" alt="Olive" />*/}
+        <br />
+        <h1 className="text-5xl text-black font-mono font-extrabold">
+          Olive's Portfolio
+        </h1>
+        <br />
+        <p>err... sorry, there's nothing here right now :/</p>
+        <img src={pookie} className="scale-50" alt="Pookie the cat :3" />
+      </div>
+    </>
+  );
+}
+
+export default App;
