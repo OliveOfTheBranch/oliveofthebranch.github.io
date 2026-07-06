69e63cc4e0541a3b4ed4f1b47b9170dfdd280adc
OliveOfTheBranch <williamdk.101@gmail.com>
2026-07-06 22:24:24 +1200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import pookie from "./assets/pookiegrab.png";
+//import title from "./assets/title.png";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        {/*<img src={title} className="size-52" alt="Olive" />*/}
-        <br />
-        <h1 className="text-5xl text-black font-mono font-extrabold">
+      <div className="flex flex-col items-center">
+        {/*<img src={title} className="w-100 h-auto" alt="Olive" />*/}
+        <h1 className="text-5xl pt-8 text-black font-mono font-extrabold">
           Olive's Portfolio
         </h1>
-        <br />
-        <p>err... sorry, there's nothing here right now :/</p>
-        <img src={pookie} className="scale-50" alt="Pookie the cat :3" />
+        <p className="pt-8">err... sorry, there's nothing here right now :/</p>
+        <img
+          src={pookie}
+          className="pt-8 w-40 h-auto"
+          alt="Pookie the cat :3"
+        />
       </div>
     </>
   );
